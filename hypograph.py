53730bfc3afc161a825dbf15c74ca224163c20f6
Duncan Fife <doctorafternoon@gmail.com>
2022-09-03 22:49:29 +0100

    scaleparam

--- hypograph.py
+++ hypograph.py
@@ -115,4 +115,63 @@ class ScaleBox(ToolPanel):
         self.uparrow = wx.Bitmap(iconpath + "/uparrow.png", wx.BITMAP_TYPE_PNG)
         self.downarrow = wx.Bitmap(iconpath + "/downarrow.png", wx.BITMAP_TYPE_PNG)
 
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        vbox.AddSpacer(5)
+        self.vconbox = wx.BoxSizer(wx.VERTICAL)
+
+        for graphpanel in parent.panelset:
+            self.AddGraphConsole(graphpanel)
+		    #gsync[i] = NULL;
+            
+        vbox.Add(self.vconbox, 1)
+
         wx.StaticText(self, label='ScaleBox')
+
+
+    def AddGraphConsole(self, graphpanel):
+        psetbox = wx.BoxSizer(wx.VERTICAL)
+        graphpanel.consolebox = wx.BoxSizer(wx.VERTICAL)
+        zoombox = wx.BoxSizer(wx.HORIZONTAL)
+
+        graphpanel.yf = self.AddScaleParam("YF", 0, psetbox, graphpanel.index)
+        graphpanel.yt = self.AddScaleParam("YT", 10, psetbox, graphpanel.index)
+        graphpanel.xf = self.AddScaleParam("XF", 0, psetbox, graphpanel.index)
+        graphpanel.xt = self.AddScaleParam("XT", 500, psetbox, graphpanel.index)
+
+        graphpanel.yzoomin = wx.BitmapButton(self, 1000 + graphpanel.index, self.downarrow, wx.DefaultPosition, wx.Size(20, 20))
+        graphpanel.yzoomout = wx.BitmapButton(self, 1010 + graphpanel.index, self.uparrow, wx.DefaultPosition, wx.Size(20, 20))
+        graphpanel.xzoomin = wx.BitmapButton(self, 1100 + graphpanel.index, self.leftarrow, wx.DefaultPosition, wx.Size(20, 20))
+        graphpanel.xzoomout = wx.BitmapButton(self, 1110 + graphpanel.index, self.rightarrow, wx.DefaultPosition, wx.Size(20, 20))
+
+        zoombox.Add(graphpanel.yzoomin, 0, wx.ALIGN_CENTRE_HORIZONTAL|wx.ALIGN_CENTRE_VERTICAL|wx.ALL, 0)
+        zoombox.Add(graphpanel.yzoomout, 0, wx.ALIGN_CENTRE_HORIZONTAL|wx.ALIGN_CENTRE_VERTICAL|wx.ALL, 0)
+        zoombox.AddSpacer(2)
+        zoombox.Add(graphpanel.xzoomin, 0, wx.ALIGN_CENTRE_HORIZONTAL|wx.ALIGN_CENTRE_VERTICAL|wx.ALL, 0)
+        zoombox.Add(graphpanel.xzoomout, 0, wx.ALIGN_CENTRE_HORIZONTAL|wx.ALIGN_CENTRE_VERTICAL|wx.ALL, 0)
+
+        psetbox.Add(zoombox, 0, wx.ALIGN_CENTRE_HORIZONTAL)
+        graphpanel.consolebox.Add(psetbox, 0, wx.ALIGN_CENTRE_HORIZONTAL)
+        self.vconbox.Add(graphpanel.consolebox, 1, wx.ALIGN_CENTRE_HORIZONTAL, 0)
+
+
+    def AddScaleParam(self, label, initval, psetbox, index):
+            boxwidth = 45
+            boxheight = -1
+            boxgap = 2
+
+            snum = "{}".format(initval)
+            confont = wx.Font(wx.FontInfo(8).FaceName("Tahoma"))
+            pbox = wx.BoxSizer(wx.HORIZONTAL)
+            label = wx.StaticText(self.panel, wx.ID_STATIC, label, wx.DefaultPosition, wx.Size(-1, -1), 0)
+
+            numbox = TextBox(self.panel, wx.ID_ANY, snum, wx.DefaultPosition, wx.Size(boxwidth, boxheight), wx.TE_PROCESS_ENTER)
+            label.SetFont(confont)
+            numbox.SetFont(confont)
+            pbox.Add(label, 0, wx.ALIGN_CENTER_VERTICAL|wx.RIGHT, 2)
+            pbox.Add(numbox, 0, wx.ALIGN_CENTER_VERTICAL, 0)
+
+            psetbox.Add(pbox, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, boxgap)
+            numbox.Bind(wx.EVT_SET_FOCUS, self.OnConFocus, self)
+            numbox.val = gpos
+            
+            return numbox
